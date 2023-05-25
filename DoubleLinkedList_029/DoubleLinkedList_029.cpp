f0954b2dffcec13d73dbfb80039aa3ccfd83bc88
RamaSandhy <rshandy07@gmail.com>
2023-05-25 11:49:38 +0700

    Telusuri daftar tertaut ganda dalam urutan menurun dan cetak catatan di DoubleLinkedList::descending()

--- DoubleLinkedList_029/DoubleLinkedList_029.cpp
+++ DoubleLinkedList_029/DoubleLinkedList_029.cpp
@@ -103,12 +103,25 @@ bool DoubleLinkedList::listEmpty() {
 
 void DoubleLinkedList::ascending() {
 	if (listEmpty())
-		cout << "\nRecords in descending order of roll number are;" << endl;
+		cout << "\nList is empty" << endl;
 	else {
 		Node* currentNode = START;
+		cout << "\nRecords in descending order of roll number are;" << endl;
 		while (currentNode->next != NULL) {
 			cout << currentNode->noMhs << " " << currentNode->name << endl;
 			currentNode = currentNode->next;
 		}
+	}
+}
+
+void DoubleLinkedList::descending() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
 	}
 }
